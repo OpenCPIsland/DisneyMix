9f95b472f104bef5cc9998cb27fa793f2fa44e15
Vurzk <141588603+vurzk-dev@users.noreply.github.com>
2026-05-27 05:34:53 -0500

    Refactor AvatarElement properties and constructor

--- Assets/Scripts/Assembly-CSharp/Avatar/DataTypes/AvatarElement.cs
+++ Assets/Scripts/Assembly-CSharp/Avatar/DataTypes/AvatarElement.cs
@@ -1,23 +1,26 @@
 namespace Avatar.DataTypes
 {
-    public class AvatarElement : BaseEntitlement
-    {
-        public int    ReferenceId   { get; private set; }
-        public int    DefaultTint   { get; private set; }
-        public string Category      { get; private set; }
-        public bool   IsRandomizable { get; private set; }
+	public class AvatarElement : BaseEntitlement
+	{
+		public int ReferenceId;
 
-        public AvatarElement(string aId, int aReferenceId, string aName, string aCategory, string aSd, string aHd, string aThumb, bool aIsRandomizable, int aDefaultTint)
-        {
-            Id             = aId;
-            ReferenceId    = aReferenceId;
-            Name           = aName;
-            Category       = aCategory;
-            Sd             = aSd;
-            Hd             = aHd;
-            Thumb          = aThumb;
-            IsRandomizable = aIsRandomizable;
-            DefaultTint    = aDefaultTint;
-        }
-    }
+		public int DefaultTint;
+
+		public string Category { get; private set; }
+
+		public bool IsRandomizable { get; private set; }
+
+		public AvatarElement(string aId, int aReferenceId, string aName, string aCategory, string aSd, string aHd, string aThumb, bool aIsRandomizable, int aDefaultTint)
+		{
+			base.Id = aId;
+			ReferenceId = aReferenceId;
+			base.Name = aName;
+			Category = aCategory;
+			base.Sd = aSd;
+			base.Hd = aHd;
+			base.Thumb = aThumb;
+			IsRandomizable = aIsRandomizable;
+			DefaultTint = aDefaultTint;
+		}
+	}
 }
